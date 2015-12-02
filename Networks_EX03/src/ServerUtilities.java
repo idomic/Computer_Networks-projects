07fab387cd205f8e71bce25ed04424b2336e8b6e
Ido Michael <Idomic@Idos-MacBook-Pro-2.local>
2015-12-02 14:27:25 +0200

    Added server Utils

---
+++ Networks_EX03/src/ServerUtilities.java
@@ -0,0 +1,13 @@
+
+class ServerUtilities {
+
+	public ServerUtilities() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected String getFileType(String i_type) {
+		
+		return null;
+	}
+
+}
