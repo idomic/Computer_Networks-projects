4ef321f9613dea1b58d53eaf7abc26d5e9db356a
Ido Michael <Idomic@Idos-MacBook-Pro-3.local>
2015-12-07 15:38:35 +0200

    added half of architecture

--- Networks_EX03/src/ServerUtilities.java
+++ Networks_EX03/src/ServerUtilities.java
@@ -1,13 +1,27 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 
 class ServerUtilities {
 
+	private Map<String, String> m_dictionary;
+	private static boolean m_isFileValid;
 	public ServerUtilities() {
 		// TODO Auto-generated constructor stub
+	}
+	private void parseConfigFile(File i_configFile) {
+		m_dictionary = new HashMap<String, String>();
+		for
+	
 	}
 	
 	protected String getFileType(String i_type) {
 		
 		return null;
+	}
+	protected void checkFile(File i_fileToCheck) {
+		m_isFileValid = i_fileToCheck.exists() && !i_fileToCheck.isDirectory() && i_fileToCheck.canRead();
 	}
 
 }
